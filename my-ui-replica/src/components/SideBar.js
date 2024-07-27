453f609a8ba8f72378d2aa19f581a4f3366d10ee
matthew-modica <mmodica29@gmail.com>
2024-07-27 10:47:42 -0400

    Style Sidebar using TailwindCSS

--- my-ui-replica/src/components/SideBar.js
+++ my-ui-replica/src/components/SideBar.js
@@ -1,14 +1,40 @@
 import Link from 'next/link';
-import styles from './Sidebar.module.css';
 
 const SideBar = () => {
     return (
-        <div className={styles.sidebar}>
-            <div className={styles.logo}>Unititled UI</div>
-            <nav className={styles.nav}>
-                <Link href="/">Home</Link>
-                <Link href="/">All Files</Link>
+        <div className="bg-gray-900 text-white h-full w-64 p-4 flex flex-col">
+            <div className="text-2xl font-bold mb-6">Untitled UI</div>
+            <nav className="flex-1">
+                <ul>
+                    <li className="mb-4">
+                        <Link href="/" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <span className="ml-2">Home</span>
+                        </Link>
+                    </li>
+                    <li className="mb-4">
+                        <Link href="/" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <span className="ml-2">All Files</span>
+                        </Link>
+                    </li>
+                    {/* Add more navigation items here */}
+                </ul>
             </nav>
+            <div className="mt-auto">
+                <ul>
+                    <li className="mb-4">
+                        <Link href="/" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <span className="ml-2">Settings</span>
+                        </Link>
+                    </li>
+                </ul>
+                <div className="text-sm text-gray-400 mt-6">
+                    <div>Storage</div>
+                    <div className="bg-gray-800 h-2 rounded mt-1">
+                        <div className="bg-blue-500 h-full rounded" style={{ width: '92%' }}></div>
+                    </div>
+                    <div className="mt-1">9.2 GB of 10 GB used</div>
+                </div>
+            </div>
         </div>
     );
 };
