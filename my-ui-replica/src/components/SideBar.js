1efff089a3e7d9c876c266059d1df029591674b0
matthew-modica <mmodica29@gmail.com>
2024-07-31 14:23:53 -0400

    Add Sidebar icons

--- my-ui-replica/src/components/SideBar.js
+++ my-ui-replica/src/components/SideBar.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { FaHome, FaFileAlt, FaLock, FaShareAlt, FaTrash, FaPalette, FaBell, FaCog } from 'react-icons/fa';
 
 const SideBar = () => {
     return (
@@ -8,41 +9,49 @@ const SideBar = () => {
                 <ul>
                     <li className="mb-4">
                         <Link href="/" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <FaHome />
                             <span className="ml-2">Home</span>
                         </Link>
                     </li>
                     <li className="mb-4">
                         <Link href="/all-files" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <FaFileAlt />
                             <span className="ml-2">All Files</span>
                         </Link>
                     </li>
                     <li className="mb-4">
                         <Link href="/private-files" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <FaLock />
                             <span className="ml-2">Private Files</span>
                         </Link>
                     </li>
                     <li className="mb-4">
                         <Link href="/shared-with-me" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <FaShareAlt />
                             <span className="ml-2">Shared With Me</span>
                         </Link>
                     </li>
                     <li className="mb-4">
                         <Link href="/deleted-files" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <FaTrash />
                             <span className="ml-2">Deleted Files</span>
                         </Link>
                     </li>
                     <li className="mb-4">
                         <Link href="/design" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <FaPalette />
                             <span className="ml-2">Design</span>
                         </Link>
                     </li>
                     <li className="mb-4">
                         <Link href="/notifications" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <FaBell />
                             <span className="ml-2">Notifications</span>
                         </Link>
                     </li>
                     <li className="mb-4">
                         <Link href="/settings" className="flex items-center p-2 hover:bg-gray-700 rounded">
+                            <FaCog />
                             <span className="ml-2">Settings</span>
                         </Link>
                     </li>
